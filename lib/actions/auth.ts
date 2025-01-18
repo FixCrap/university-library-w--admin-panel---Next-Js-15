7e10253ca829d9a80ddd85bc6eafb75f6da4f755
FixCrap <george_andriopoulos@hotmail.gr>
2025-01-18 18:51:01 +0200

    Implement Upstash and ratelimit

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -6,9 +6,9 @@ import { users } from "@/database/schema";
 import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
 import { headers } from "next/headers";
-//import ratelimit from "@/lib/ratelimit";
+import ratelimit from "@/lib/ratelimit";
 import { redirect } from "next/navigation";
-//import { workflowClient } from "@/lib/workflow";
+import { workflowClient } from "@/lib/workflow";
 import config from "@/lib/config";
 
 export const signInWithCredentials = async (
@@ -16,10 +16,10 @@ export const signInWithCredentials = async (
 ) => {
 	const { email, password } = params;
 
-	// const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
-	// const { success } = await ratelimit.limit(ip);
+	const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+	const { success } = await ratelimit.limit(ip);
 
-	//if (!success) return redirect("/too-fast");
+	if (!success) return redirect("/too-fast");
 
 	try {
 		const result = await signIn("credentials", {
