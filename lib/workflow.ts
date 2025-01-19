a1efe1951a4b62fb731f5ffae644be4a2bec6903
FixCrap <george_andriopoulos@hotmail.gr>
2025-01-19 16:52:20 +0200

    change eimail in workflow

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({
 			provider: resend({ token: config.env.resendToken }),
 		},
 		body: {
-			from: "JS Mastery <hello.george-andriopoulos.com>",
+			from: "George Andriopoulos <onboarding@resend.dev>",
 			to: [email],
 			subject,
 			html: message,
