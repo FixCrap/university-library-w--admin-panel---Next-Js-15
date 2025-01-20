bfe5fc744960a1a2209cfbbd2a9d9c4c2bc43431
FixCrap <george_andriopoulos@hotmail.gr>
2025-01-20 09:56:35 +0200

    Implement display books on main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,33 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
-import { sampleBooks } from "@/constants";
 import { db } from "@/database/drizzle";
-import { users } from "@/database/schema";
-import React from "react";
+import { books, users } from "@/database/schema";
+import { auth } from "@/auth";
+import { desc } from "drizzle-orm";
 
-async function Home() {
-	const result = await db.select().from(users);
-	console.log(JSON.stringify(result, null, 2));
+const Home = async () => {
+	const session = await auth();
+
+	const latestBooks = (await db
+		.select()
+		.from(books)
+		.limit(10)
+		.orderBy(desc(books.createdAt))) as Book[];
 
 	return (
 		<>
-			<BookOverview {...sampleBooks[0]} />
+			<BookOverview
+				{...latestBooks[0]}
+				userId={session?.user?.id as string}
+			/>
 
 			<BookList
 				title='Latest Books'
-				books={sampleBooks}
+				books={latestBooks.slice(1)}
 				containerClassName='mt-28'
 			/>
 		</>
 	);
-}
+};
 
 export default Home;
